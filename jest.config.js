d654be4d3b8b7f9b2d228ae6059bc533830b0f84
Simon Msara <simonmsaraone@gmail.com>
2020-04-21 16:04:57 +0100

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {
+        '^.+\\.ts?$': 'ts-jest'
+    },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx|js)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        "^~/(.*)$": "<rootDir>/src/$1"
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+
+    },
+    resolve: {
+        extensions: [ '.ts' ],
+        alias: {
+            '~': path.resolve(__dirname, 'src')
+        }
+    },
+    devtool: 'inline-source-map',
+};
