fb67d806f84cb418252608e333003637bb7b6dd6
Simon Msara <simonmsaraone@gmail.com>
2020-04-22 22:49:00 +0100

    Changed the module generator to be a module utilities class

---
+++ src/ModuleUtilities.ts
@@ -0,0 +1,76 @@
+import * as D from '~/declarations'
+
+class ModuleUtilities {
+    private _module: D.Module = {
+        namespaced: true, state: {}, getters: {}, mutations: {}, actions: {}, modules: {}
+    };
+
+    constructor(namespaced: boolean = true) {
+        this.namespaced = namespaced
+    }
+
+    get module(): D.ExportModule {
+        return {
+            namespaced: this._module.namespaced,
+            state: () => this._module.state,
+            getters: this._module.getters,
+            mutations: this._module.mutations,
+            actions: this._module.actions,
+            modules: this._module.modules
+        }
+    }
+    set module(value) { this._module = value }
+
+    get namespaced(): boolean { return this._module.namespaced}
+    set namespaced(value: boolean) { this._module.namespaced = value }
+
+    get state() :D.Object { return this.module.state() }
+    set state(value: D.Object) { this._module.state = value }
+
+    get getters() :D.Object { return this.module.getters }
+    set getters(value: D.Object) { this._module.getters = value }
+
+    get mutations() :D.Object { return this.module.mutations }
+    set mutations(value: D.Object) { this._module.mutations = value }
+
+    get actions() :D.Object { return this.module.actions }
+    set actions(value: D.Object) { this._module.actions = value }
+
+    get modules() :D.Object { return this.module.modules }
+    set modules(value: D.Object) { this._module.modules = value }
+
+    reset(): void {
+        this._module = {
+            namespaced: this.namespaced, state: {}, getters: {}, mutations: {}, actions: {}, modules: {}
+        }
+    }
+    resetState(): void { this._module.state = {} }
+    resetGetters(): void { this._module.getters = {} }
+    resetMutations(): void { this._module.mutations = {} }
+    resetActions(): void { this._module.actions = {} }
+    resetModules(): void { this._module.modules = {} }
+
+    addState(key: string, value: any): void {
+        this.state[key] = value
+    }
+    addGetter(key: string, value: (state: object, getters?: object) => any): void {
+        this.getters[key] = value
+    }
+    addMutation(key: string, value: (state: object, payload?: any) => void): void {
+        this.mutations[key] = value
+    }
+    addAction(key: string, value: (context: object, payload?: any) => void): void {
+        this.actions[key] = value
+    }
+    addModule(key: string, value: D.ExportModule): void {
+        this.modules[key] = value
+    }
+
+    removeState(key: string): void { delete this.state[key] }
+    removeGetter(key: string): void { delete this.getters[key]}
+    removeMutation(key: string): void { delete this.mutations[key]}
+    removeAction(key: string): void { delete this.actions[key]}
+    removeModule(key: string): void { delete this.modules[key]}
+}
+
+export default ModuleUtilities
---
+++ tests/ModuleUtilities.spec.ts
@@ -0,0 +1,186 @@
+import ModuleUtilities from "~/ModuleUtilities";
+
+describe('store/ModuleUtilities/ModuleUtilities.ts', () => {
+    let utilities: ModuleUtilities;
+    const test_filled_module = {
+        namespaced: false,
+        state() {return { key: 'value' }},
+        getters: { key: 'value' },
+        mutations: { key: 'value' },
+        actions: { key: 'value' },
+        modules: { key: 'value' }
+    };
+
+    beforeEach(() => {
+        utilities = new ModuleUtilities();
+    });
+
+    test('The utilities can be instantiated', () => {
+        expect( () => { new ModuleUtilities }).not.toThrow();
+    });
+
+    test('The utilities has a module property of type object', () => {
+        expect(typeof utilities.module).toEqual('object');
+    });
+
+    test('The utilities has a namespaced property of type boolean', () => {
+        expect(typeof utilities.namespaced).toEqual('boolean');
+    });
+
+    test('The namespaced property is set to true by default but can be set to false on instantiation', () => {
+        expect(utilities.namespaced).toBe(true);
+        const test_utilities = new ModuleUtilities(false);
+        expect(test_utilities.namespaced).toBe(false);
+    });
+
+    test('The utilities has a state property that returns an object.', () => {
+        expect(typeof utilities.state).toEqual('object');
+    });
+
+    test('The utilities has a getters property that returns an object.', () => {
+        expect(typeof utilities.getters).toEqual('object');
+    });
+
+    test('The utilities has a mutations property that returns an object.', () => {
+        expect(typeof utilities.mutations).toEqual('object');
+    });
+
+    test('The utilities has a actions property that returns an object.', () => {
+        expect(typeof utilities.actions).toEqual('object');
+    });
+
+    test('The utilities has a modules property that returns an object.', () => {
+        expect(typeof utilities.modules).toEqual('object');
+    });
+
+    test('Resetting the utilities keeps the value of namespaced property', () => {
+        const test_obj = {name: 'First Name'};
+        const test_utilities = new ModuleUtilities(false);
+
+        test_utilities.state = test_obj;
+        expect(test_utilities.module.namespaced).toEqual(false);
+        expect(test_utilities.module.state()).toEqual(test_obj);
+
+        test_utilities.reset();
+        expect(test_utilities.module.namespaced).toEqual(false);
+        expect(test_utilities.module.state()).toEqual({});
+    });
+
+    test('The state can be reset', () => {
+        utilities.module = test_filled_module;
+        utilities.resetState();
+        expect(utilities.module.state()).toEqual({});
+        expect(utilities.module.getters).toEqual(test_filled_module.getters);
+        expect(utilities.module.mutations).toEqual(test_filled_module.mutations);
+        expect(utilities.module.actions).toEqual(test_filled_module.actions);
+        expect(utilities.module.modules).toEqual(test_filled_module.modules);
+    });
+
+    test('The getters can be reset', () => {
+        utilities.module = test_filled_module;
+        utilities.resetGetters();
+        expect(utilities.module.state()).toEqual(test_filled_module.state);
+        expect(utilities.module.getters).toEqual({});
+        expect(utilities.module.mutations).toEqual(test_filled_module.mutations);
+        expect(utilities.module.actions).toEqual(test_filled_module.actions);
+        expect(utilities.module.modules).toEqual(test_filled_module.modules);
+    });
+
+    test('The mutations can be reset', () => {
+        utilities.module = test_filled_module;
+        utilities.resetMutations();
+        expect(utilities.module.state()).toEqual(test_filled_module.state);
+        expect(utilities.module.getters).toEqual(test_filled_module.getters);
+        expect(utilities.module.mutations).toEqual({});
+        expect(utilities.module.actions).toEqual(test_filled_module.actions);
+        expect(utilities.module.modules).toEqual(test_filled_module.modules);
+    });
+
+    test('The actions can be reset', () => {
+        utilities.module = test_filled_module;
+        utilities.resetActions();
+        expect(utilities.module.state()).toEqual(test_filled_module.state);
+        expect(utilities.module.getters).toEqual(test_filled_module.getters);
+        expect(utilities.module.mutations).toEqual(test_filled_module.mutations);
+        expect(utilities.module.actions).toEqual({});
+        expect(utilities.module.modules).toEqual(test_filled_module.modules);
+    });
+
+    test('The modules can be reset', () => {
+        utilities.module = test_filled_module;
+        utilities.resetModules();
+        expect(utilities.module.state()).toEqual(test_filled_module.state);
+        expect(utilities.module.getters).toEqual(test_filled_module.getters);
+        expect(utilities.module.mutations).toEqual(test_filled_module.mutations);
+        expect(utilities.module.actions).toEqual(test_filled_module.actions);
+        expect(utilities.module.modules).toEqual({});
+    });
+
+    test('State properties can be added and removed', () => {
+        const test_object = {key: 'last_name', value: 'Example'};
+        // Add
+        utilities.addState(test_object.key, test_object.value);
+        expect(utilities.state[test_object.key]).toEqual(test_object.value);
+        // Remove
+        utilities.removeState(test_object.key);
+        expect(utilities.state[test_object.key]).toBeUndefined();
+    });
+
+    test('Getters can be added and removed', () => {
+        const test_object = {
+            key: 'getLastName',
+            value: (state: {[x: string]: any}) => {
+                return state.last_name
+            }
+        };
+        // Add
+        utilities.addGetter(test_object.key, test_object.value);
+        expect(utilities.getters[test_object.key]).toEqual(test_object.value);
+        // Remove
+        utilities.removeGetter(test_object.key);
+        expect(utilities.getters[test_object.key]).toBeUndefined();
+    });
+
+    test('Mutations can be added and removed', () => {
+        const test_object = {
+            key: 'setLastName',
+            value: (state: {[x: string]: any}, value: string) => {
+                return state.last_name = value.toUpperCase()
+            }
+        };
+        // Add
+        utilities.addMutation(test_object.key, test_object.value);
+        expect(utilities.mutations[test_object.key]).toEqual(test_object.value);
+        // Remove
+        utilities.removeMutation(test_object.key);
+        expect(utilities.mutations[test_object.key]).toBeUndefined();
+    });
+
+    test('Actions can be added and removed', () => {
+        const test_object = {
+            key: 'setLastName',
+            value: (context: {[x: string]: any}): void => {
+                return context.commit('setLastName')
+            }
+        };
+        // Add
+        utilities.addAction(test_object.key, test_object.value);
+        expect(utilities.actions[test_object.key]).toEqual(test_object.value);
+        // Remove
+        utilities.removeAction(test_object.key);
+        expect(utilities.actions[test_object.key]).toBeUndefined();
+    });
+
+    test('Modules can be added and removed', () => {
+        const test_object = {
+            key: 'setLastName',
+            value: test_filled_module
+        };
+        // Add
+        utilities.addModule(test_object.key, test_object.value);
+        expect(utilities.modules[test_object.key]).toEqual(test_object.value);
+        // Remove
+        utilities.removeModule(test_object.key);
+        expect(utilities.modules[test_object.key]).toBeUndefined();
+    });
+});
