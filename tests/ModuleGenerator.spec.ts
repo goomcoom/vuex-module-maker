77d3e83e612083528e00ee6336dc8c28e4494edb
Simon Msara <simonmsaraone@gmail.com>
2020-04-22 23:22:09 +0100

    Passing an empty object returns an empty module

--- tests/ModuleGenerator.spec.ts
+++ tests/ModuleGenerator.spec.ts
@@ -1,6 +1,9 @@
 import ModuleGenerator from "~/ModuleGenerator";
 
 describe('src/ModuleGenerator.ts', () => {
-    let generator: ModuleGenerator
-
+    test('Passing an empty raw module returns an empty module', () => {
+        const generator = new ModuleGenerator;
+        const module  = JSON.stringify(generator.module);
+        expect(JSON.stringify(generator.generate({}))).toEqual(module);
+    });
 });
