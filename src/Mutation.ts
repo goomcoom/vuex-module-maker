7aac1ba100cd191b9b1c116a04820007031571d6
Simon Msara <simonmsaraone@gmail.com>
2020-04-26 22:56:53 +0100

    Removed all value validations from the mutations

--- src/Mutation.ts
+++ src/Mutation.ts
@@ -12,72 +12,10 @@ class Mutation<S> {
 
     format <T extends D.Types>(type: T): VMutation<S>
     {
-        switch (type) {
-            case 'string':
-                return this.stringMutation as VMutation<S>;
-            case 'number':
-                return this.numberMutation as VMutation<S>;
-            case 'boolean':
-                return this.booleanMutation as VMutation<S>;
-            case 'object':
-                return this.objectMutation as VMutation<S>;
-            case 'array':
-                return this.arrayMutation as VMutation<S>;
-            case 'form':
-                return this.formMutation as VMutation<S>;
-            default:
-                return this.anyMutation as VMutation<S>;
-        }
-    }
-
-    stringMutation: D.StringMutation = <S>(state: S, value?: string): void => {
-        // @ts-ignore
-        state[this.state_name] = value ? value : null
-    };
-
-    numberMutation: D.NumberMutation = <S>(state: S, value?: number): void => {
-        // @ts-ignore
-        state[this.state_name] = value == null ? null : value
-    };
-
-    booleanMutation: D.BooleanMutation = <S>(state: S, value?: any): void => {
-        // @ts-ignore
-        state[this.state_name] = value == null ? null : !!value
-    };
-
-    objectMutation: D.ObjectMutation = <S>(state: S, value?: object): void => {
-        if (value == null || Object.keys(value).length === 0) {
+        return <S>(state: S, value?: D.Type<T>): void => {
             // @ts-ignore
-            state[this.state_name] = null
-        } else {
-            // @ts-ignore
-            state[this.state_name] = value
-        }
-    };
-
-    arrayMutation: D.ArrayMutation = <S>(state: S, value?: any[]): void => {
-        if (value == null || value.length === 0) {
-            // @ts-ignore
-            state[this.state_name] = null
-        } else {
-            // @ts-ignore
-            state[this.state_name] = value
-        }
-    };
-
-    formMutation: D.FormMutation = <S>(state: S, value?: D.Object): void => {
-        if (value == null || Object.keys(value).length === 0) {
-            // @ts-ignore
-            state[this.state_name] = null
-        } else {
-            // @ts-ignore
-            state[this.state_name] = new Form(value)
-        }
-    };
-
-    anyMutation: D.AnyMutation = <S>(state: S, value?: any): void => {
-        // @ts-ignore
-        state[this.state_name] = value === undefined ? null : value
+            state[this.state_name] = (value == null) ? null : value;
+        };
     }
 }
 
--- tests/Mutation.spec.ts
+++ tests/Mutation.spec.ts
@@ -4,6 +4,7 @@ import Mutation from "~/Mutation";
 import InstructionProcessor from "~/InstructionProcessor";
 
 describe('store/ModuleGenerator/Mutation.ts', () => {
+
     test('The set_mutation property can be controlled', () => {
         // Default
         let processor = new InstructionProcessor({comments:{type: 'array'}});
@@ -21,102 +22,24 @@ describe('store/ModuleGenerator/Mutation.ts', () => {
         expect(processor.process()[0].mutation_name).toEqual('setUserId')
     });
 
-    test.each([['string'],['number'],['boolean'],['object'],['array'],['form'],['any']])
-    (`Setting the type as %s returns the correct mutation`, (type) => {
-        // @ts-ignore
-        const raw = new Mutation('name');
-        // @ts-ignore
-        expect(raw.format(type)).toEqual(raw[`${type}Mutation`])
-    });
-
-    test.each([['string'],['number'],['boolean'],['object'],['array'],['form'],['any']])
-    (`The %s raw set undefined and null values as null`, (type) => {
+    test('If no value is passed the state property is set as null', () => {
         let state = {name: null};
         // @ts-ignore
         const raw = new Mutation('name');
         // @ts-ignore
-        raw.format(type)(state);
+        raw.format('string')(state);
         expect(state.name).toEqual(null);
         // @ts-ignore
-        raw.format(type)(state, null);
+        raw.format('string')(state, null);
         expect(state.name).toEqual(null)
     });
 
-    test('The string mutation sets the correct value', () => {
+    test('The mutation sets the state prop equal to the value if passed', () => {
         let state = {name: null};
         const raw = new Mutation('name');
         const mutation = raw.format('string');
 
-        mutation(state, '');
-        expect(state.name).toEqual(null);
-
         mutation(state, 'example');
         expect(state.name).toEqual('example')
-    });
-
-    test('The number mutation sets the correct value', () => {
-        let state = {name: null};
-        const raw = new Mutation('name');
-        const mutation = raw.format('number');
-
-        mutation(state, 0);
-        expect(state.name).toEqual(0);
-
-        mutation(state, 234567);
-        expect(state.name).toEqual(234567)
-    });
-
-    test('The boolean mutation sets the correct value', () => {
-        let state = {name: null};
-        const raw = new Mutation('name');
-        const mutation = raw.format('boolean');
-
-        mutation(state, false);
-        expect(state.name).toEqual(false);
-
-        mutation(state, true);
-        expect(state.name).toEqual(true);
-    });
-
-    test('The object mutation sets the correct value', () => {
-        let state = {profile: null};
-        const raw = new Mutation('profile');
-        const mutation = raw.format('object');
-
-        mutation(state, {});
-        expect(state.profile).toEqual(null);
-
-        const test_profile = {
-            name: 'First Name',
-            age: 22
-        };
-        mutation(state, test_profile);
-        expect(state.profile).toEqual(test_profile);
-    });
-
-    test('The array mutation sets the correct value', () => {
-        let state = {words: null};
-        const raw = new Mutation('words');
-        const mutation = raw.format('array');
-
-        mutation(state, []);
-        expect(state.words).toEqual(null);
-
-        const test_words = ['some', 'test', 'words'];
-        mutation(state, test_words);
-        expect(state.words).toEqual(test_words);
-    });
-
-    test('The form mutation sets the correct value', () => {
-        let state = {words: null};
-        const raw = new Mutation('words');
-        const mutation = raw.format('form');
-
-        mutation(state, {});
-        expect(state.words).toEqual(null);
-
-        const test_words = {old: ['some', 'words']};
-        mutation(state, test_words);
-        expect(state.words).toEqual(new Form(test_words));
     });
 });
