69a1e383df8b3feb22a7817fdbb78a9e5eb3978d
Simon Msara <simonmsaraone@gmail.com>
2020-04-27 21:27:00 +0100

    Added the variable type Ts to the Getter class

--- src/Getter.ts
+++ src/Getter.ts
@@ -1,7 +1,7 @@
 import * as D from "../types";
 import { Getter as VGetter } from "vuex";
 
-class Getter<S, R> {
+class Getter<S, R, Ts> {
     readonly _config: D.Config<S, R>;
     get config() { return this._config; };
 
@@ -9,19 +9,22 @@ class Getter<S, R> {
         this._config = config;
     }
 
-    format  <T extends D.Types> (type: T, state_name: string, default_value: any = undefined): VGetter<S, R>
+    format  <T extends D.Types<Ts>> (type: T, state_name: string, default_value: any = undefined): VGetter<S, R>
     {
         default_value = (default_value === undefined) ? this.getDefaultValue(type) : default_value;
         return this.prepareGetter(type, state_name, default_value);
     }
 
-    getDefaultValue(type: string): any {
+    getDefaultValue <T extends D.Types<Ts>>(type: T): any {
+        // @ts-ignore
         if (this.config.types[type] === undefined) return this.config.types.default.default_value;
+        // @ts-ignore
         if (this.config.types[type].default_value !== undefined) return this.config.types[type].default_value;
         return this.config.types.default.default_value;
     }
 
-    prepareGetter(type: string, state_name: string, default_value: any): VGetter<S, R> {
+    prepareGetter <T extends D.Types<Ts>>(type: T, state_name: string, default_value: any): VGetter<S, R> {
+        // @ts-ignore
         if (this.config.types[type] === undefined || this.config.types[type].getter === undefined) {
             return this.config.types.default.getter(state_name, default_value);
         }
