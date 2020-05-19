312811b83630659ea878442920f2659d6444eaee
Simon Msara <simonmsaraone@gmail.com>
2020-05-19 20:14:09 +0100

    Removed the unused type variables from the ModuleMaker class

--- src/ModuleMaker.ts
+++ src/ModuleMaker.ts
@@ -1,60 +1,48 @@
 import * as D from "../types";
 import ModuleUtilities from "./ModuleUtilities";
 import InstructionProcessor from "./InstructionProcessor";
-import {ActionTree, GetterTree, Module, MutationTree} from "vuex";
 import {Config, Template} from "../types";
 
-class ModuleMaker<S, R, Ts> extends ModuleUtilities<R> {
+class ModuleMaker<M, Ts> extends ModuleUtilities {
 
-    constructor(template: Template<S, R, Ts>, config: D.CustomConfig<R> = {})
+    constructor(config: D.CustomConfig = {})
     {
         super(config);
-        this.make<S>(template)
-
-        // @ts-ignore
-        return {
-            namespaced: this.namespaced,
-            state: () => this.state,
-            getters: this.getters,
-            mutations: this.mutations,
-            actions: this.actions,
-            modules: this.modules,
-        };
     }
 
-    make<S>(template: D.Template<S, R, Ts>): any
+    make<M, Ts>(template: D.Template<Ts>): M
     {
         this.reset()
 
         if (template.namespaced != null) this.namespaced = template.namespaced;
-        if (template.instructions) this.executeInstructions<S>(template.instructions);
-        if (template.state) this.addStateProperties<S>(template.state);
-        if (template.getters) this.addGetters<S>(template.getters);
-        if (template.mutations) this.addMutations<S>(template.mutations);
-        if (template.actions) this.addActions<S>(template.actions);
-        if (template.modules) this.addModules<S>(template.modules);
+        if (template.instructions) this.executeInstructions(template.instructions);
+        if (template.state) this.addStateProperties(template.state);
+        if (template.getters) this.addGetters(template.getters);
+        if (template.mutations) this.addMutations(template.mutations);
+        if (template.actions) this.addActions(template.actions);
+        if (template.modules) this.addModules(template.modules);
 
         return this.module;
     }
-    public static Make<Module, Types>(template: Template<Types>, config?: Config): Module
+    public static Make<M, Ts>(template: Template<Ts>, config?: Config): M
     {
-        const maker = new ModuleMaker(config);
-        return maker.make(template) as Module;
+        const maker = new ModuleMaker<M, Ts>(config);
+        return maker.make(template);
     }
 
-    private executeInstructions<S>(raw: D.Instructions<S, R, Ts>): void
+    private executeInstructions(raw: D.Instructions<Ts>): void
     {
-        const processor = new InstructionProcessor<S, R, Ts>(raw, this.config);
+        const processor = new InstructionProcessor<Ts>(raw, this.config);
         const instructions = processor.process();
 
-        instructions.forEach((i: D.FormattedInstruction<Ts, S, R, Ts>) => {
+        instructions.forEach((i: D.FormattedInstruction<Ts, Ts>) => {
             if (i.set_state) this.addState(i.state_name, i.state_value);
             if (i.set_getter) this.addGetter(i.getter_name, i.getter);
             if (i.set_mutation) this.addMutation(i.mutation_name, i.mutation);
         });
     }
 
-    private addStateProperties<S>(properties: Object | (() => S)): void
+    private addStateProperties(properties: Object | (() => {})): void
     {
         if (typeof properties === 'function') properties = properties();
 
@@ -63,31 +51,31 @@ class ModuleMaker<S, R, Ts> extends ModuleUtilities<R> {
         }
     }
 
-    private addGetters<S>(getters: GetterTree<S, R>): void
+    private addGetters(getters: { [x:string]: ()=>any }): void
     {
         for (const [key, value] of Object.entries(getters)) {
             this.addGetter(key, value);
         }
     }
 
-    private addMutations<S>(mutations: MutationTree<S>): void
+    private addMutations(mutations: { [x:string]: ()=>void }): void
     {
         for (const [key, value] of Object.entries(mutations)) {
             this.addMutation(key, value);
         }
     }
 
-    private addActions<S>(actions: ActionTree<S, R>): void
+    private addActions(actions: { [x:string]: ()=>any }): void
     {
         for (const [key, value] of Object.entries(actions)) {
             this.addAction(key, value);
         }
     }
 
-    private addModules<S>(modules: Module<S, R>): void
+    private addModules(modules: { [x:string]: any }): void
     {
         for (const [key, value] of Object.entries(modules)) {
-            this.addModule(key, new ModuleMaker<S, R, Ts>(value, this.config));
+            this.addModule(key, new ModuleMaker<M, Ts>(value, this.config));
         }
     }
 }
