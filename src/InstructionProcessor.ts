3b6965dd78396163283970ea9e13b656d89cdc28
Simon Msara <simonmsaraone@gmail.com>
2020-05-19 23:15:20 +0100

    Removed unused types from the InstructionProcessor class

--- src/InstructionProcessor.ts
+++ src/InstructionProcessor.ts
@@ -1,30 +1,29 @@
 import * as D from "../types";
 import Getter from "./Getter";
 import Mutation from "./Mutation";
-import {Getter as VGetter, Mutation as VMutation} from "vuex";
 
-class InstructionProcessor<S, R, Ts> {
-    readonly _raw: D.Instructions<S, R, Ts>;
+class InstructionProcessor<Ts> {
+    readonly _raw: D.Instructions<Ts>;
     get raw() { return this._raw }
 
-    readonly _config: D.Config<R>;
+    readonly _config: D.Config;
     get config() { return this._config }
 
-    private _instructions: D.FormattedInstructions<S, R, Ts> = [];
+    private _instructions: D.FormattedInstructions<Ts> = [];
     get instructions() { return this._instructions }
 
     private _state_name: string = '';
     get state_name() { return this._state_name }
     set state_name(value) { this._state_name = value }
 
-    constructor (instructions: D.Instructions<S, R, Ts>, config: D.Config<R>) {
+    constructor (instructions: D.Instructions<Ts>, config: D.Config) {
         this._raw = instructions;
         this._config = config;
     }
 
-    process(): D.FormattedInstructions<S, R, Ts> {
+    process(): D.FormattedInstructions<Ts> {
         let name: string;
-        let options: Ts | D.Instruction<Ts, S, R, Ts>;
+        let options: Ts | D.Instruction<Ts,Ts>;
         for ([name, options] of Object.entries(this.raw)) {
             if (typeof options === 'string') options = {type: options};
             // @ts-ignore
@@ -34,7 +33,7 @@ class InstructionProcessor<S, R, Ts> {
         return this.instructions
     }
 
-    processInstruction <T extends Ts> (name: string, options: D.Instruction<T, S, R, Ts>): D.FormattedInstruction<T, S, R, Ts>
+    processInstruction <T extends Ts> (name: string, options: D.Instruction<T, Ts>): D.FormattedInstruction<T, Ts>
     {
         return {
             type: options.type,
@@ -53,14 +52,14 @@ class InstructionProcessor<S, R, Ts> {
         }
     }
 
-    formatStateName <T extends Ts> (name: string, options: D.Instruction<T, S, R, Ts>): string {
+    formatStateName <T extends Ts> (name: string, options: D.Instruction<T, Ts>): string {
         if (options.state_name) return this.state_name = options.state_name;
 
         const config = this.config.naming.state;
         return this.state_name = config.transformer(config.prefix + name + config.suffix);
     }
 
-    formatStateValue <T extends Ts> (options: D.Instruction<T, S, R, Ts>): D.Type<T>|null {
+    formatStateValue <T extends Ts> (options: D.Instruction<T, Ts>): D.Type<T>|null {
         if (options.initial_value !== undefined) return options.initial_value;
         // @ts-ignore
         if (this.config.types[options.type] == null || this.config.types[options.type].initial_value === undefined) {
@@ -70,32 +69,30 @@ class InstructionProcessor<S, R, Ts> {
         return this.config.types[options.type].initial_value;
     }
 
-    formatGetterName <T extends Ts> (name: string, options: D.Instruction<T, S, R, Ts>): string {
+    formatGetterName <T extends Ts> (name: string, options: D.Instruction<T, Ts>): string {
         if (options.getter_name) return options.getter_name;
 
         const config = this.config.naming.getter;
         return config.transformer(config.prefix + name + config.suffix);
     }
 
-    formatGetter <T extends Ts> (options: D.Instruction<T, S, R, Ts>): VGetter<S, R> {
+    formatGetter <T extends Ts> (options: D.Instruction<T, Ts>): ()=>any {
         if (options.getter) return options.getter;
         const getters = new Getter(this.config);
-
         // @ts-ignore
-        return getters.format(options.type, this.state_name, options.default_value)
+        return getters.format(options.type, this.state_name, options.default_value);
     }
 
-    formatMutationName <T extends Ts> (name: string, options: D.Instruction<T, S, R, Ts>): string {
+    formatMutationName <T extends Ts> (name: string, options: D.Instruction<T, Ts>): string {
         if (options.mutation_name) return options.mutation_name;
-
         const config = this.config.naming.mutation;
         return config.transformer(config.prefix + name + config.suffix);
     }
 
-    formatMutation <T extends Ts> (type: T): VMutation<S> {
-        const raw = new Mutation<S, R, Ts>(this.config);
-        return raw.format(type, this.state_name)
+    formatMutation <T extends Ts> (type: T): ()=>void {
+        const raw = new Mutation<Ts>(this.config);
+        return raw.format(type, this.state_name);
     }
 }
 
-export default InstructionProcessor
+export default InstructionProcessor;
